494c33716ca7f8bb1ee76b9685fd23e1f2ec75d7
Aleksei Mialkin <anmyalkin@gmail.com>
2020-10-28 01:01:35 +0300

    Gets DICT_BACKUP_OAUTH_TOKEN environment variable

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DictionaryBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? oAuthToken = Environment.GetEnvironmentVariable("DICT_BACKUP_OAUTH_TOKEN");
+            if(string.IsNullOrEmpty(oAuthToken))
+                throw new ArgumentNullException(nameof(oAuthToken));
+
+            PrintMessage("Starting backup.");
+            Backup(oAuthToken);
+
+            Console.WriteLine($"{oAuthToken}");
+        }
+
+        static void Backup(string token)
+        {
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine($"{DateTime.Now:yyyy-dd-M--HH-mm-ss} — {message}");
+        }
+    }
+}
