4cc77e7d068e139979d33a7b5cfa03deaa8923ab
Aleksei Mialkin <anmyalkin@gmail.com>
2020-12-13 19:43:45 +0300

    Removes IHostBuilder

--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Slova.Backuper.FileReader;
 using Slova.Backuper.FileUploader;
 using Slova.Backuper.Settings;
@@ -14,30 +13,29 @@ namespace Slova.Backuper
     {
         static async Task Main(string[] args)
         {
-            using IHost host = CreateHostBuilder(args).Build();
+            ServiceCollection services = new();
+            ConfigureServices(services);
 
-            App app = host.Services.GetRequiredService<App>();
-            await app.Run();
-
-            await host.RunAsync();
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            await serviceProvider.GetService<App>()!.Run();
         }
 
-        static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host
-                .CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, builder) => { builder.AddEnvironmentVariables(prefix: "SLOVA_BACKUPER_"); })
-                .ConfigureServices((context, serviceCollection) =>
-                {
-                    serviceCollection
-                        .AddSingleton(new HttpClient { BaseAddress = new Uri("https://cloud-api.yandex.net/v1/disk/") })
-                        .AddTransient<IFileReader, FileReader.FileReader>()
-                        .AddTransient<IFileUploader, FileUploader.FileUploader>()
-                        .AddTransient<App>();
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables("SLOVA_BACKUPER_")
+                .Build();
 
-                    serviceCollection.AddOptions<FileReaderSettings>().Bind(context.Configuration.GetSection(FileReaderSettings.FileReader))
-                        .ValidateDataAnnotations();
-                    serviceCollection.AddOptions<FileUploaderSettings>().Bind(context.Configuration.GetSection(FileUploaderSettings.FileUploader))
-                        .ValidateDataAnnotations();
-                });
+            services
+                .AddSingleton(new HttpClient { BaseAddress = new Uri("https://cloud-api.yandex.net/v1/disk/") })
+                .AddTransient<IFileReader, FileReader.FileReader>()
+                .AddTransient<IFileUploader, FileUploader.FileUploader>()
+                .AddTransient<App>();
+
+            services.AddOptions<FileReaderSettings>().Bind(configuration.GetSection(FileReaderSettings.FileReader))
+                .ValidateDataAnnotations();
+            services.AddOptions<FileUploaderSettings>().Bind(configuration.GetSection(FileUploaderSettings.FileUploader))
+                .ValidateDataAnnotations();
+        }
     }
 }
